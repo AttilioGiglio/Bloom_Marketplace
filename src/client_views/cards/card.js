74c6783cd301db9ffae373fead5c8ab2e9e140d9
attiliogiglio <attilio.g93@gmail.com>
2020-10-01 09:36:52 -0300

    addind css to products view

--- src/client_views/cards/card.js
+++ src/client_views/cards/card.js
@@ -6,7 +6,7 @@ const Card = ({common_name, image_url}) => {
         <>
         <div className='d-inline-flex m-5'> 
             <div className="card" style={{width:'18rem'}}>
-                <img className="card-img-top" src={image_url} alt="Card" />
+                <img className="card-img-top" src={image_url} alt="Card"/>
                 <div className="card-body">
                     <h5 className="card-title">{common_name}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
