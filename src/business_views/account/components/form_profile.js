933536de05b5920a12b32c0efe44df30e028648b
attiliogiglio <attilio.g93@gmail.com>
2020-10-06 17:27:25 -0300

    adding account config view

--- src/business_views/account/components/form_profile.js
+++ src/business_views/account/components/form_profile.js
@@ -1,40 +1,85 @@
-import React from 'react'
+import React from 'react';
+import { AiOutlineEdit } from 'react-icons/ai';
 
 const FormProfile = () => {
     return (
         <div>
             <form className='card p-4' style={{ margin: '50px auto 0', width: '80%', minHeight: '50vh' }}>
-                <div class="form-row">
-                    <div class="col-md-3 mb-3">
-                        <label for="validationDefault01">Categoria</label>
-                        <input type="text" class="form-control" id="validationDefault01" value="Mark" required />
+                <fieldset>
+                    <legend>Datos de cuenta</legend>
+                    <div class="form-row">
+                        <div class="col-md-6 mb-3">
+                            <label for="validationDefault01">Nombre de usuario</label>
+                            <input type="text" class="form-control" id="validationDefault01" value="Mark" required />
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="validationDefault01">Correo</label>
+                            <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                        </div>
                     </div>
-                    <div class="col-md-3 mb-3">
-                        <label for="validationDefault01">Nombre Producto</label>
-                        <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                </fieldset>
+                <fieldset>
+                    <legend>Datos personales</legend>
+                    <div class="form-row">
+                        <div class="col-md-6 mb-3">
+                            <label for="validationDefault02">Nombre y apellido</label>
+                            <input type="text" class="form-control" id="validationDefault01" value="Mark" required />
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="validationDefault02">Rut</label>
+                            <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                        </div>
                     </div>
-                    <div class="col-md-3 mb-3">
-                        <label for="validationDefault01">Cantidad</label>
-                        <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                    <div class="form-row">
+                        <div class="col-md-6 mb-3">
+                            <label for="validationDefault02">Telefono</label>
+                            <input type="text" class="form-control" id="validationDefault01" value="Mark" required />
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="validationDefault02">Domicilio</label>
+                            <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                        </div>
                     </div>
-                    <div class="col-md-3 mb-3">
-                        <label for="validationDefault01">Precio</label>
-                        <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                </fieldset>
+                <fieldset>
+                    <legend>Datos de facturación</legend>
+                    <div class="form-row">
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault03">Razon Social</label>
+                            <input type="text" class="form-control" id="validationDefault01" value="Mark" required />
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault03">Rut empresa</label>
+                            <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault03">Domicilio</label>
+                            <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                        </div>
                     </div>
-                </div>
-                <div class="form-row">
-                    <div class="col-md-12 mb-3">
-                        <label for="validationDefault03">Descripción</label>
-                        <input type="text" class="form-control" id="validationDefault03" required />
+                </fieldset>
+                <fieldset>
+                    <legend>Tarjeta de pago</legend>
+                    <div class="form-row">
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault04">Nombre titular</label>
+                            <input type="text" class="form-control" id="validationDefault01" value="Mark" required />
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault04">Numero de tarjeta</label>
+                            <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault04">CVV</label>
+                            <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault04">Fecha de expiración</label>
+                            <input type="text" class="form-control" id="validationDefault02" value="Otto" required />
+                        </div>
                     </div>
-                </div>
-                <div class="form-group">
-                    <div class="form-group">
-                        <label for="exampleFormControlFile1">Imagen de tu producto</label>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1" required />
-                    </div>
-                </div>
-                <button class="btn" style={{ width: '15%', background: '#479A79', border: 'solid 1px #479A79', borderRadius: '5px 5px 5px 5px', color: '#fcf9f3', fontSize: '20px' }} type="submit">Submit form</button>
+                </fieldset>
+                <button class="btn mt-3" style={{ width: '15%', background: '#479A79', border: 'solid 1px #479A79', borderRadius: '5px 5px 5px 5px', color: '#fcf9f3', fontSize: '20px' }} type="submit">Editar <AiOutlineEdit className='mb-1' style={{fontSize: '21px'}}/></button>
             </form>
         </div>
     )
