80031eb37598c925da33f89b5f560dcd25176166
attiliogiglio <attilio.g93@gmail.com>
2020-10-22 22:38:20 -0300

    adding config api

---
+++ src/store/config.js
@@ -0,0 +1,12 @@
+export const APIS = {
+    GETUSERS: 'http://localhost:5000/users',
+    GETUSER: 'http://localhost:5000/user', //aqui va el id del user
+    CREATEUSER: 'http://localhost:5000/register', 
+    LOGIN: 'http://localhost:5000/login',
+    USERTOKEN: 'http://localhost:5000/token', 
+    GETPRODUCTS: 'http://localhost:5000/products',
+    GETPRODUCT: 'http://localhost:5000/product',
+    GETPRODUCTSBYUSER: 'http://localhost:5000/products/user',
+    CREATEPRODUCT: 'http://localhost:5000/product',
+    DELETEPRODUCT: 'http://localhost:5000/product',
+}
